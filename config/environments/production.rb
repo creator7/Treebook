02566e306abd5b0055ce76817ddeb0414de3701e
creator7 <abdullabintariq@gmail.com>
2015-09-15 11:08:23 +0500

    Slightly changes production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,11 +20,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: ENV["GMAIL_DOMAIN"],
+      domain: "gmail.com",
       authentication: "plain",
       enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
+      user_name: "abdullabintariq@gmail.com",
+      password: "Swetoo@@6939"
   }
 
   # Full error reports are disabled and caching is turned on.
